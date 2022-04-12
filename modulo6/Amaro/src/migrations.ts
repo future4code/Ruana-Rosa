62cd6503d7e952efc473805171c2a718a5515afc
Ruanar <ruana.piber@gmail.com>
2022-04-11 23:47:10 -0300

    projeto amaro iniciado

---
+++ modulo6/Amaro/src/migrations.ts
@@ -0,0 +1,13 @@
+connection
+    .raw(`
+CREATE TABLE IF NOT EXISTS amaroProducts (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR (255) NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS amaroTags (
+    productId VARCHAR(255),
+    FOREING KEY (productId) references amaroProducts(id),
+    name VARCHAR (255) NOT NULL 
+  );
+`)
